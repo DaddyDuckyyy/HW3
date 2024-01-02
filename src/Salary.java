8f8e8178bf4c5d2226ad6711bbe488d6ffb4a906
Daria Orlova <da.orlova365@gmail.com>
2024-01-02 07:31:38 +0300

    1.1

--- src/Salary.java
+++ src/Salary.java
@@ -11,8 +11,8 @@ public class Salary {
         System.out.println("Маша теперь получает " + mashaMonthlySalary*1.1f + " рублей. " +
                 "Годовой доход вырос на " + (mashaAnnualSalary*0.1f) +" рублей");
         System.out.println("Денис теперь получает " + denisMonthlySalary*1.1f + " рублей. " +
-                "Годовой доход вырос на " + (denisAnnualSalary*0.1f)+" рублей");
+                "Годовой доход вырос на " + (denisAnnualSalary*0.1f) +" рублей");
         System.out.println("Кристина теперь получает " + christineMonthlySalary*1.1f + " рублей. " +
-                "Годовой доход вырос на " + (christineAnnualSalary*0.1f)+" рублей");
+                "Годовой доход вырос на " + (christineAnnualSalary*0.1f) +" рублей");
     }
 }
